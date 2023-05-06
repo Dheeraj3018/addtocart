b6962bfc465add6a010c6ab2d701366966b2f12f
Dheeraj3018 <vsdheeraj17@gmail.com>
2023-05-06 11:50:24 +0530

    new1

--- src/Component/Courosel/Courosel.js
+++ src/Component/Courosel/Courosel.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel'
-import Item from './Item'
-import product from './product.json'
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import Item from "./Item";
+import product from "./product.json";
 
-
-function Example()
-{
-    return (
-        <Carousel >
-            {
-                product.map(item  => <Item key={item.id} item={item} /> )
-            }
-        </Carousel>
-    )
+function Example() {
+  return (
+    <Carousel>
+      {product.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </Carousel>
+  );
 }
 
-
-
-export default Example
+export default Example;
