393133692e1a928efd43ff6b0fdfbd3c343b0b7d
Dheeraj3018 <vsdheeraj17@gmail.com>
2023-05-06 11:46:29 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import './App.css';
+import "./App.css";
 
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './Home';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
-
   return (
-    
     <BrowserRouter>
-    <Routes>
-      {/* <Route path='register' element={<Register/>}/> */}
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/details" element={<ProductDetail />} /> */}
-    </Routes>
+      <Routes>
+        {/* <Route path='register' element={<Register/>}/> */}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/details" element={<ProductDetail />} /> */}
+      </Routes>
     </BrowserRouter>
   );
 }
